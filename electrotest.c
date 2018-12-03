44067dbbb491a9257c9d99e74b15f347bff51a0b
adfil3 <adfil3@gmail.com>
2018-12-03 12:08:44 +0100

    Uppdaterad början på electrotest

--- electrotest.c
+++ electrotest.c
@@ -2,24 +2,46 @@
 #include "electrotest.h"
 
 int main(void){
+  char input[MAXINPUT]; // Används för att läsa input med fgets
 
-
-  printf("Ange koppling[S,P]: ");
   char conn;
-  scanf("%c",&conn);
-
-  printf("Antal komponenter: \n");
-  int count;
-  scanf("%d",&count);
-
-  float* array = calloc(count,sizeof(int));
-  for(int i = 0;i < count;i++)
-    {
-      printf("Komponent %d i ohm: ",i+1);
-      scanf("%f",array+i);
+  do{
+    printf("Ange koppling[S,P]: ");
+    fgets(input,MAXINPUT,stdin);
+    conn = *input; // Bara intresserade av första karaktären
+    if(conn != 'S' && conn != 'P'){
+      printf("Koppling får endast vara S eller P, försök igen!\n");
+      conn = 0;
     }
+  } while(conn == 0);
+
+  int count;
+  do{
+    printf("Antal komponenter: ");
+    fgets(input,MAXINPUT,stdin);
+    count = atoi(input);
+    if(count < 1){
+      printf("Antal komponenter får inte vara mindre än 1, försök igen!\n");
+    }
+  } while(count < 1);
+  
+  float* array = calloc(count,sizeof(float));
+  for(int i = 0;i < count;i++){
+    do{
+      printf("Komponent %d i ohm: ",i+1);
+      fgets(input,MAXINPUT,stdin);
+      *(array+i) = atof(input);
+      if(*(array+i) < 0){
+	printf("Resistanser får inte vara negativa, försök igen!\n");
+      }      
+    } while(*(array+i) < 0);
+  }
+
   float res = calc_resistance(count,conn,array);
-  printf("Ersättningsresistans: %.1f ohm\n",calc_resistance(count,conn,array)); // Skriv resultat med 1 decimal
+  if(res == -1)
+    printf("Nåt gick fel i beräkningen, konstigt...\n");
+  else
+    printf("Ersättningsresistans: %.1f ohm\n",res); // Skriv resultat med 1 decimal
 
   printf("Ange spänningen: ");
   float voltage;
